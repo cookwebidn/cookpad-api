50d59aa7a0bcc567cad48687ce287d4bc590868e
Harisatul <haris.aulia404@gmail.com>
2023-02-01 22:31:56 +0700

    added IngredientsTest

---
+++ src/test/java/com/cookpadidn/entity/IngredientsTest.java
@@ -0,0 +1,17 @@
+package com.cookpadidn.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class IngredientsTest {
+
+
+    @Test
+    void ingredientsTest() {
+        UUID uuid = UUID.randomUUID();
+        String ingredientName = "Mentega";
+        UOM ounch = UOM.ounch;
+
+    }
+}
