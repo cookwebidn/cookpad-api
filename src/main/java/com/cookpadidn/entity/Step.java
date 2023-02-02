d831cf34cb46162c0be5be1f0b23a78ec56cc5b3
Harisatul <haris.aulia404@gmail.com>
2023-02-02 15:19:58 +0700

    make a constructor and relation to recipe

--- src/main/java/com/cookpadidn/entity/Step.java
+++ src/main/java/com/cookpadidn/entity/Step.java
@@ -1,10 +1,13 @@
 package com.cookpadidn.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,6 +16,13 @@ import java.util.List;
 @NoArgsConstructor
 public class Step extends AbstractAuditableEntity {
 
+    public Step(String steps, List<String> photoUrls) {
+        this.steps = steps;
+        this.photoUrls = photoUrls;
+    }
+
     private String steps;
-    private List<String> photoURls;
+    private List<String> photoUrls = new ArrayList<>();
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Recipe recipe;
 }
