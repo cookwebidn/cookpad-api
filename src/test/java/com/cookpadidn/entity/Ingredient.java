6f458fcecf8939bc6da30e4e4326ba28a434d909
Harisatul <haris.aulia404@gmail.com>
2023-02-02 13:49:59 +0700

    added Ingredient entity

---
+++ src/test/java/com/cookpadidn/entity/Ingredient.java
@@ -0,0 +1,17 @@
+package com.cookpadidn.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ingredient extends AbstractAuditableEntity{
+
+    private String ingredient;
+    private UOM unitOfMeasure;
+
+}
