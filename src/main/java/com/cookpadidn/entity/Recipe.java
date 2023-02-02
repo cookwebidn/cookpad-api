c67394459b81295997770be311e835411a86cf32
Harisatul <haris.aulia404@gmail.com>
2023-02-02 14:40:43 +0700

    refactoring code. success

--- src/main/java/com/cookpadidn/entity/Recipe.java
+++ src/main/java/com/cookpadidn/entity/Recipe.java
@@ -1,15 +1,29 @@
 package com.cookpadidn.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Recipe extends AbstractAuditableEntity {
+
+    public Recipe(String photoUrl, RecipeTag recipeTag) {
+        this.photoUrl = photoUrl;
+        this.recipeTag = recipeTag;
+    }
+
     private String photoUrl;
     private RecipeTag recipeTag;
+
+    @OneToMany(mappedBy = "recipe")
+    private List<Ingredient> ingredients = new ArrayList<>();
+
 }
