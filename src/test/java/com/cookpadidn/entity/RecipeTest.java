2a50656f5c708019978f025cfc7cf8bc24b9983c
Harisatul <haris.aulia404@gmail.com>
2023-02-02 14:21:08 +0700

    make test faiil

--- src/test/java/com/cookpadidn/entity/RecipeTest.java
+++ src/test/java/com/cookpadidn/entity/RecipeTest.java
@@ -4,6 +4,7 @@ package com.cookpadidn.entity;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import java.util.UUID;
 
 
@@ -28,11 +29,15 @@ public class RecipeTest {
         UUID uuid = UUID.randomUUID();
         String photoUrl = "https://example.com/recipe.jpg";
         RecipeTag recipeTag = RecipeTag.APPETIZERS;
+        Short measure = 2;
+        Ingredient ingredient1 = new Ingredient("Sugar", measure, UOM.TEASPOON);
+        Ingredient ingredient2 = new Ingredient("Sugar", measure, UOM.TEASPOON);
 
-        Ingredient ingredient = new Ingredient("Sugar", 2 ,UOM.TEASPOON);
+        List<Ingredient> ingredients = List.of(ingredient1, ingredient2);
 
         Recipe recipe = new Recipe("https://example.com/recipe.jpg", RecipeTag.APPETIZERS);
         recipe.setId(uuid);
+        recipe.setIngredients(ingredients);
 
     }
 }
