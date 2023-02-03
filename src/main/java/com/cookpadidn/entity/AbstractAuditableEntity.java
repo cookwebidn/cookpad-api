16750ca2f5f593536ac65013e2f6d5ef606cdaea
Harisatul <haris.aulia404@gmail.com>
2023-02-03 11:17:38 +0700

    added pre persist, preupdate, pre remove

--- src/main/java/com/cookpadidn/entity/AbstractAuditableEntity.java
+++ src/main/java/com/cookpadidn/entity/AbstractAuditableEntity.java
@@ -1,9 +1,9 @@
 package com.cookpadidn.entity;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -11,18 +11,38 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class AbstractAuditableEntity implements Serializable {
     @Id
     private UUID id;
     @CreatedDate
-    LocalDate createDate;
+    LocalDateTime createDate;
     @LastModifiedDate
-    LocalDate lastModifiedDate;
+    LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    protected void prePersist() {
+        this.id = UUID.randomUUID();
+        if (this.createDate == null) createDate = LocalDateTime.now();
+        if (this.lastModifiedDate == null) lastModifiedDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void preUpdated(){
+        this.lastModifiedDate = LocalDateTime.now();
+    }
+
+    @PreRemove
+    protected void preRemove(){
+        this.lastModifiedDate = LocalDateTime.now();
+    }
 
 }
