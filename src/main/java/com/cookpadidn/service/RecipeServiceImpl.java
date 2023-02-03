e7befeb29a26b52cc9224f4ce8909de023030151
Harisatul <haris.aulia404@gmail.com>
2023-02-03 11:18:56 +0700

    implement RecipeServiceImpl

--- src/main/java/com/cookpadidn/service/RecipeServiceImpl.java
+++ src/main/java/com/cookpadidn/service/RecipeServiceImpl.java
@@ -1,25 +1,39 @@
 package com.cookpadidn.service;
 
 import com.cookpadidn.dto.RecipeRequest;
+import com.cookpadidn.entity.Ingredient;
 import com.cookpadidn.entity.Recipe;
+import com.cookpadidn.entity.Step;
+import com.cookpadidn.repository.IngredientRepository;
 import com.cookpadidn.repository.RecipeRepository;
+import com.cookpadidn.repository.StepRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    private final IngredientRepository ingredientRepository;
+    private final StepRepository stepRepository;
+    public RecipeServiceImpl(RecipeRepository recipeRepository, IngredientRepository ingredientRepository, StepRepository stepRepository) {
         this.recipeRepository = recipeRepository;
+        this.ingredientRepository = ingredientRepository;
+        this.stepRepository = stepRepository;
     }
 
     @Override
     public Recipe addRecipe(RecipeRequest recipeRequest) {
+        List<Ingredient> ingredients = recipeRequest.getIngredients();
+        List<Ingredient> savedIngredient = ingredientRepository.saveAllAndFlush(ingredients);
+        List<Step> savedSteps = stepRepository.saveAll(recipeRequest.getSteps());
         Recipe recipe = Recipe.builder().photoUrl(recipeRequest.getPhotoUrl())
                 .recipeTag(recipeRequest.getRecipeTag())
-                .ingredients(recipeRequest.getIngredients())
-                .steps(recipeRequest.getSteps()).build();
+                .ingredients(savedIngredient)
+                .steps(savedSteps).build();
+//        savedIngredient.forEach(ingredient -> ingredient.setRecipe(recipe));
         return recipeRepository.save(recipe);
     }
 }
