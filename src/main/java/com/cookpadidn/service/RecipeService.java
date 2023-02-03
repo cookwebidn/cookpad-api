bff5d42b954b77bc2cf5069a44045ea604871705
Harisatul <haris.aulia404@gmail.com>
2023-02-03 15:03:43 +0700

    defined RecipeService

--- src/main/java/com/cookpadidn/service/RecipeService.java
+++ src/main/java/com/cookpadidn/service/RecipeService.java
@@ -3,8 +3,9 @@ package com.cookpadidn.service;
 import com.cookpadidn.dto.RecipeRequest;
 import com.cookpadidn.entity.Recipe;
 
+
 public interface RecipeService {
 
     Recipe addRecipe(RecipeRequest recipeRequest);
-
+    Recipe getRecipeById(String id);
 }
