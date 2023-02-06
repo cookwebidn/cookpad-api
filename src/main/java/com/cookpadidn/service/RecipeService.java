cce9e717f10be334a895730fb69cc5081c6cdd1a
Harisatul <haris.aulia404@gmail.com>
2023-02-06 09:26:14 +0700

    defined updateRecipe

--- src/main/java/com/cookpadidn/service/RecipeService.java
+++ src/main/java/com/cookpadidn/service/RecipeService.java
@@ -8,4 +8,6 @@ public interface RecipeService {
 
     Recipe addRecipe(RecipeRequest recipeRequest);
     Recipe getRecipeById(String id);
+    Recipe updateRecipe(RecipeRequest recipeRequest);
+
 }
