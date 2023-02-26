c0ded9b395aa287415cee023ebe155c6036b35ad
mfyasykur <mf.yasykur@gmail.com>
2023-02-26 11:51:26 +0700

    remove unused import

--- src/main/java/com/cookpadidn/dto/UserRequest.java
+++ src/main/java/com/cookpadidn/dto/UserRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
